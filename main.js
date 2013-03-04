eb66d3732911cadcd7d78a1dd7ba9ab9f0dee6e1
Chia-Yen Hung <chiayen.hung@nyu.edu>
2013-03-04 01:26:43 -0500

    integrate facebook and twitter share link

--- main.js
+++ main.js
@@ -22,12 +22,5 @@ var AppRouter = Backbone.Router.extend({
     $(page.el).attr('id', viewName);
     page.render();
     $('body').append($(page.el));
-    // var transition = $.mobile.defaultPageTransition;
-    //     // We don't want to slide the first page
-    //     if (this.firstPage) {
-    //         transition = 'none';
-    //         this.firstPage = false;
-    //     }
-    //     $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
     },
 })
